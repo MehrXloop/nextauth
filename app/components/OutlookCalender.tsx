83f0f1108d3546d44d4c5a924d17b911f8f62da9
Mehrunissa Pirwani (2303.KHI.CND.007) <mehrunissa.pirwani@xloopdigital.com>
2023-12-02 18:52:27 +0500

    modal work complete

--- app/components/OutlookCalender.tsx
+++ app/components/OutlookCalender.tsx
@@ -8,8 +8,8 @@ import 'moment-timezone';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
-import { Popover, PopoverTrigger, PopoverContent, PopoverBody, Button } from '@chakra-ui/react';
-
+import { EditIcon, SmallCloseIcon, ChatIcon, InfoOutlineIcon,ArrowRightIcon, DeleteIcon,TimeIcon, CalendarIcon } from '@chakra-ui/icons'
+import { Text, Modal, ModalOverlay, ModalContent, ModalHeader, Textarea, ModalBody, ModalCloseButton, Button, Heading, Divider } from '@chakra-ui/react';
 
 
 interface CalendarEvent {
@@ -31,8 +31,10 @@ interface OutlookCalendarProps {
 
 const OutlookCalendar: React.FC<OutlookCalendarProps> = ({ accessToken }) => {
     const [events, setEvents] = useState<CalendarEvent[]>([]);
-    const [clickedEvent, setClickedEvent] = useState<CalendarEvent | null>(null);
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
     const [clickedEventInfo, setClickedEventInfo] = useState<any>(null);
+    const [cancellationNote, setCancellationNote] = useState('');
+    const [cancelMode, setCancelMode] = useState(false)
 
 
     const fetchEventsForMonth = async (start: Date, end: Date) => {
@@ -106,6 +108,9 @@ const OutlookCalendar: React.FC<OutlookCalendarProps> = ({ accessToken }) => {
     }, []);
 
     // const handleEventClick = (clickInfo: any) => {
+
+
+
     //     // const eventId = clickInfo.event._def.extendedProps.eventId;
     //     // const organizer = clickInfo.event._def.extendedProps.isOrganizer
 
@@ -151,21 +156,75 @@ const OutlookCalendar: React.FC<OutlookCalendarProps> = ({ accessToken }) => {
     // };
 
 
+    // Function to handle opening the modal and setting clicked event info
     const handleEventClick = (clickInfo: any) => {
         setClickedEventInfo(clickInfo);
-        console.log(clickedEventInfo)
+        console.log(clickInfo.event._def.extendedProps.bodyPreview)
+        setIsModalOpen(true); // Open the modal
     };
 
-    const closePopover = () => {
-        setClickedEventInfo(null);
+    // Function to close the modal
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setClickedEventInfo(null); // Reset clicked event info
     };
 
-    function cancelEvent(id: string) {
-        console.log('cancel button clicked', id)
+    function cancelEvent() {
+        setCancelMode(true)
     }
-    function joinMeeting(id: string) {
-        console.log('join button clicked', id)
+    async function cancelEventButton(id: string) {
+        const eventId = id;
+        const url = `https://graph.microsoft.com/v1.0/me/events/${eventId}/cancel`;
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ comment: cancellationNote }),
+            });
+            if (response.ok) {
+                console.log('Event cancellation successful!');
+                const updatedEvents = events.filter((event) => event.eventId !== eventId);
+                setEvents(updatedEvents); // Update the state or refetch events
+                // Close the modal or perform any other action upon cancellation success
+                setIsModalOpen(false);
+                setCancellationNote(''); // Reset the cancellation note
+            } else {
+                console.error('Event cancellation failed:', response.statusText);
+                // Handle failure, show error message, etc.
+            }
+        } catch (error) {
+            console.error('Error occurred during cancellation:', error);
+            // Handle any other errors
+        }
     }
+
+
+
+
+    function joinMeeting(url: string) {
+        window.open(url, '_blank');
+    }
+
+
+    function editEvent(id: string) {
+        console.log('edit button clicked', id)
+    }
+
+    function formatDateTime(start: any, end: any) {
+        const startDate = start.toLocaleDateString('en-US', { weekday: 'short', month: 'numeric', day: 'numeric', year: 'numeric' });
+        const startTime = start.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+        const endTime = end.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+
+        return `${startDate} ${startTime} - ${endTime}`;
+    }
+    const extractBodyContent = (bodyPreview: string) => {
+        return bodyPreview.split('___')[0];
+    };
+
     return (
         <div style={{ width: '90vw', margin: 'auto' }}>
             <FullCalendar
@@ -188,33 +247,124 @@ const OutlookCalendar: React.FC<OutlookCalendarProps> = ({ accessToken }) => {
             />
 
 
+            {/* Chakra UI Modal */}
             {clickedEventInfo && (
-                <Popover isOpen={!!clickedEventInfo} onClose={closePopover}>
-                    <PopoverTrigger>
-                        <div>
-                            {/* This could be your clicked event */}
-                            <h5>{clickedEventInfo.event._def.extendedProps.title}</h5>
-                        </div>
-                    </PopoverTrigger>
-                    <PopoverContent>
-                        <PopoverBody>
-                            <h5>{clickedEventInfo.event._def.title}</h5>
-                            <p>Start Date: {(clickedEventInfo.range)}</p>
-                            <Button colorScheme="blue" onClick={() => joinMeeting(clickedEventInfo.event._def.extendedProps.eventId)}>
-                                Join Meeting
-                            </Button>
-                            {clickedEventInfo.event._def.extendedProps.isOrganizer && (
-                                <Button colorScheme="red" onClick={() => cancelEvent(clickedEventInfo.event._def.extendedProps.eventId)}>
-                                    Cancel Event
-                                </Button>
-                            )}
-                        </PopoverBody>
-                    </PopoverContent>
-                </Popover>
+                <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>{clickedEventInfo.event._def.extendedProps.title}</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            {
+                                clickedEventInfo.event._def.extendedProps.isOrganizer ? (
+                                    <>
+                                        <Heading as='h5' size='lg' textAlign='center' >{clickedEventInfo.event._def.title}</Heading>
+                                        <Divider />
+                                        <Text> <TimeIcon mr={4} /> {formatDateTime(clickedEventInfo.event._instance.range.start, clickedEventInfo.event._instance.range.end)}</Text>
+                                        {clickedEventInfo?.event?._def?.extendedProps?.onlineMeeting?.joinUrl &&
+                                            <>
+                                                <Divider />
+                                                <Text fontSize='lg'><CalendarIcon mr={4} /> Microsoft Teams Meeting</Text>
+                                                <Button ml={8} p={5} colorScheme="blue" color="black" leftIcon={<CalendarIcon />} onClick={() => joinMeeting(clickedEventInfo?.event?._def?.extendedProps?.onlineMeeting?.joinUrl)}>
+                                                    Join
+                                                </Button>
+                                            </>
+                                        }
+                                        <Divider />
+                                        <Text><InfoOutlineIcon mr={4} />You&rsquo;re the organizer.</Text>
+                                        <Divider />
+                                        {clickedEventInfo.event._def.extendedProps.bodyPreview && (
+                                            (() => {
+                                                const extractedContent = extractBodyContent(clickedEventInfo.event._def.extendedProps.bodyPreview);
+                                                if (extractedContent) {
+                                                    return (
+                                                        <>
+                                                            <Text>
+                                                                <ChatIcon mr={4} /> {extractedContent}
+                                                            </Text>
+                                                            <Divider />
+                                                        </>
+                                                    );
+                                                }
+                                                return null;
+                                            })()
+                                        )}
+
+                                        <Button ml={4} leftIcon={<EditIcon color='blue' />} mb={4} colorScheme='black' mr={4} variant='outline' onClick={() => editEvent(clickedEventInfo.event._def.extendedProps.eventId)}>
+                                            Edit Event
+                                        </Button>
+                                        <Button leftIcon={<SmallCloseIcon color='red' />} mb={4} colorScheme='black' variant='outline' onClick={() => cancelEvent()}>
+                                            Cancel Event
+                                        </Button>
+                                        {cancelMode && (
+                                            <>
+                                                <Textarea
+                                                    value={cancellationNote}
+                                                    onChange={(e) => setCancellationNote(e.target.value)}
+                                                    placeholder="Enter cancellation note..."
+                                                    mb={4}
+                                                />
+                                                <Button
+                                                    colorScheme="blue"
+                                                    color="black"
+                                                    mr={3}
+                                                    leftIcon={<ArrowRightIcon  />}
+                                                    onClick={() => {
+                                                        cancelEventButton(clickedEventInfo.event._def.extendedProps.eventId);
+                                                        setIsModalOpen(false);
+                                                        setCancelMode(false);
+                                                    }}
+                                                >
+                                                    Send
+                                                </Button>
+                                                <Button variant="outline" color="black" leftIcon={<DeleteIcon />} onClick={() => setCancelMode(false)}>Discard</Button>
+                                            </>
+                                        )}
+                                    </>
+                                ) : (
+                                    <>
+                                        <Heading as='h5' size='lg' textAlign='center' >{clickedEventInfo.event._def.title}</Heading>
+                                        <Divider />
+                                        <Text> <TimeIcon mr={4} /> {formatDateTime(clickedEventInfo.event._instance.range.start, clickedEventInfo.event._instance.range.end)}</Text>
+                                        {clickedEventInfo?.event?._def?.extendedProps?.onlineMeeting?.joinUrl &&
+                                            <>
+                                                <Divider />
+                                                <Text fontSize='lg'><CalendarIcon mr={4} /> Microsoft Teams Meeting</Text>
+                                                <Button ml={8} p={5} colorScheme="blue" color="black" leftIcon={<CalendarIcon />} onClick={() => joinMeeting(clickedEventInfo?.event?._def?.extendedProps?.onlineMeeting?.joinUrl)}>
+                                                    Join
+                                                </Button>
+                                            </>
+                                        }
+                                        <Divider />
+
+                                        {clickedEventInfo.event._def.extendedProps.bodyPreview && (
+                                            (() => {
+                                                const extractedContent = extractBodyContent(clickedEventInfo.event._def.extendedProps.bodyPreview);
+                                                if (extractedContent) {
+                                                    return (
+                                                        <>
+                                                            <Text>
+                                                                <ChatIcon mr={4} /> {extractedContent}
+                                                            </Text>
+                                                            <Divider />
+                                                        </>
+                                                    );
+                                                }
+                                                return null;
+                                            })()
+                                        )}
+                                        <Text mb={4}><InfoOutlineIcon mr={4} />{clickedEventInfo.event._def.extendedProps.organizer.emailAddress.name} invited you</Text>
+                                    </>
+                                )
+                            }
+
+                        </ModalBody>
+                    </ModalContent>
+                </Modal>
             )}
 
         </div>
     );
-};
+}
 
 export default OutlookCalendar;
